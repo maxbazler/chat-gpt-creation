bfd704ca7f810f8259174f4ffaa8143acfc13d18
Max Bazler <112581957+maxbazler@users.noreply.github.com>
2023-03-20 00:00:19 -0700

    Parallax Effect Added

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const parallax = (e) => {
+    document.querySelectorAll('.city-p').forEach((layer) => {
+        const speed = layer.getAttribute('data-speed');
+        const x = (window.innerWidth - e.pageX * speed) / 100;
+        const y = (window.innerHeight - e.pageY * speed) / 100;
+        layer.style.transform = `translateX(${x}px) translateY(${y}px) scale(1.02)`;
+    });
+};
+
+document.addEventListener('mousemove', parallax);
 (function() {
     const cursor = document.querySelector('.top-cursor');
     gsap.set(cursor, {
